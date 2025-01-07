ec3436e9fa1b8c5766478074864eae82e579ab67
Felipe Santos <owner@spacedwog.com.br>
2025-01-07 18:25:39 -0300

    Commited

---
+++ src/Dashboard.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = __importDefault(require("react"));
+const Dashboard = () => {
+    return (<div>
+      <h1>Dashboard</h1>
+      <p>Welcome to the admin panel dashboard.</p>
+    </div>);
+};
+exports.default = Dashboard;
