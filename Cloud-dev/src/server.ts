4a9e47e60ef2a627fcd666570c01c252777e32eb
Felipe Santos <owner@spacedwog.com.br>
2025-01-08 18:17:32 -0300

    Commited

---
+++ Cloud-dev/src/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+// Middleware para permitir parsing de JSON
+app.use(express.json());
+
+// Rota principal
+app.get('/', (req: Request, res: Response) => {
+    res.send('Servidor rodando em TypeScript!');
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor está rodando em http://localhost:${PORT}`);
+});
