ea4d7061cfa5120558d385a9ef75184a8dd86c78
Felipe Santos <owner@spacedwog.com.br>
2025-01-08 21:07:30 -0300

    Commited

--- Cloud-dev/src/server.ts
+++ Cloud-dev/src/server.ts
@@ -58,31 +58,32 @@ class Server {
 
         // Rota para receber dados via POST e settar nas variáveis
         this.app.post('/setDados', (req: Request, res: Response) => {
-            const { 
-                nome_paciente, 
-                nome_medico, 
-                data_prescricao, 
-                observacao, 
-                nome_medicamento, 
-                dosagem, 
-                frequencia, 
-                duracao 
-            } = req.body;
+            const dados = req.body; // Array de objetos
 
-            // Usando os setters para atribuir os valores
-            this.setNome_paciente(nome_paciente);
-            this.setNome_medico(nome_medico);
-            this.setData_prescricao(data_prescricao);
-            this.setObservacao(observacao);
-            this.setNome_medicamento(nome_medicamento);
-            this.setDosagem(dosagem);
-            this.setFrequencia(frequencia);
-            this.setDuracao(duracao);
+            // Verifica se o corpo da requisição é um array
+            if (Array.isArray(dados)) {
+                // Itera sobre cada item do array e usa os setters
+                dados.forEach(item => {
+                    this.setNome_paciente(item.nome_paciente);
+                    this.setNome_medico(item.nome_medico);
+                    this.setData_prescricao(item.data_prescricao);
+                    this.setObservacao(item.observacoes);
+                    this.setNome_medicamento(item.nome_medicamento);
+                    this.setDosagem(item.dosagem);
+                    this.setFrequencia(item.frequencia);
+                    this.setDuracao(item.duracao);
+                });
 
-            res.status(200).json({
-                message: 'Dados atualizados com sucesso!',
-                data: req.body
-            });
+                res.status(200).json({
+                    message: 'Dados atualizados com sucesso!',
+                    data: dados
+                });
+                console.log("Nome do Paciente: " + this.getNome_paciente());
+            } else {
+                res.status(400).json({
+                    error: 'O corpo da requisição deve ser um array de objetos'
+                });
+            }
         });
     }
 
@@ -110,24 +111,6 @@ class Server {
         await this.connectToDatabase();
         this.app.listen(this.port, () => {
             console.log(`Servidor está rodando em http://localhost:${this.port}`);
-            const nome_paciente = this.getNome_paciente();
-            const nome_medico = this.getNome_medico();
-            const data_prescricao = this.getData_prescricao();
-            const nome_medicamento = this.getNome_medicamento();
-            const dosagem = this.getDosagem();
-            const frequencia = this.getFrequencia();
-            const duracao = this.getDuracao();
-            console.table([
-                {
-                    "Nome do Paciente": nome_paciente,
-                    "Nome do Medico": nome_medico,
-                    "Data de Prescricao": data_prescricao,
-                    "Nome do Medicamento": nome_medicamento,
-                    "Dosagem": dosagem,
-                    "Frequencia": frequencia,
-                    "Duracao": duracao
-                }
-            ]);
         });
     }
 
