1f84e494e0cad4a6b08b6216e277e95a3e0df32a
Felipe Santos <owner@spacedwog.com.br>
2025-01-10 21:37:12 -0300

    Commited

--- Cloud-dev/src/server.ts
+++ Cloud-dev/src/server.ts
@@ -65,7 +65,7 @@ class Server {
         // Endpoint para receber os dados do Python
         this.app.post('/receive-data', (req: Request, res: Response) => {
             const data = req.body;
-            console.log("Dados recebidos da Blackboard:", data);
+            console.log("Dados recebidos da Blackboard:", data.key, data.value);
     
             // Aqui você pode processar os dados conforme necessário
             res.status(200).json({ message: 'Dados recebidos com sucesso!' });
