3f2b9ae10901daed6e5ccdd0b5a7ad8d6ddebb72
Felipe Santos <owner@spacedwog.com.br>
2025-01-14 17:38:38 -0300

    Commited

--- Cloud-dev/src/client.ts
+++ Cloud-dev/src/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getMessage = async () => {
   try {
-    const response = await axios.get("http://127.0.0.1:8000/hello");
+    const response = await axios.get("http://127.0.0.1:8001/hello");
     console.log(response.data.message);
   } catch (error) {
     console.error("Error fetching message:", error);
