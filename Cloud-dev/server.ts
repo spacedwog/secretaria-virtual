62edbeddfe3cd9ba826bdd5ad7b1c9f29bf7f1b4
Felipe Santos <owner@spacedwog.com.br>
2025-01-29 10:38:37 -0300

    Commited

--- Cloud-dev/server.ts
+++ Cloud-dev/server.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import * as express from 'express';
 import * as dotenv from 'dotenv';
 import { exec } from 'child_process';
-import * as mysql from 'mysql2';
+import * as mysql from 'mysql2/promise';
 import * as bodyParser from 'body-parser';
 import { Request, Response, NextFunction } from 'express';
 
