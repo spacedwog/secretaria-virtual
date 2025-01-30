9065e3fa458c328acf31157b86b4a899f27070f1
Felipe Santos <owner@spacedwog.com.br>
2025-01-30 14:46:56 -0300

    Commited

---
+++ dist/dist/dist/ETL/etl.js
@@ -0,0 +1,130 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) {
+            try {
+                step(generator.next(value));
+            }
+            catch (e) {
+                reject(e);
+            }
+        }
+        function rejected(value) {
+            try {
+                step(generator["throw"](value));
+            }
+            catch (e) {
+                reject(e);
+            }
+        }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { PatientService } from '../Back-end/patient.service';
+export class ETLProcess {
+    constructor() {
+        this.patientId = 0;
+        this.patientAge = 0;
+        this.patientName = "";
+        this.patientPhone = "";
+        this.patientEmail = "";
+        this.patientAddress = "";
+        this.cartaoCadastro = "";
+    }
+    gerarCartaoPaciente() {
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log('Gerando Cartão de Paciente');
+            yield this.extractDataPacient();
+        });
+    }
+    extractDataPacient() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                console.log('Extraindo Dados do Banco de Dados');
+                const patients = yield PatientService.listPatients();
+                patients.forEach((patient) => {
+                    this.setPatientId(patient.patient_id);
+                    this.setPatientName(patient.name);
+                    this.setPatientAge(patient.age);
+                    this.setPatientPhone(patient.phone);
+                    this.setPatientEmail(patient.email);
+                    this.setPatientAddress(patient.address);
+                    this.transformDataPacient();
+                });
+            }
+            catch (err) {
+                console.error('Erro ao listar pacientes:', err);
+            }
+        });
+    }
+    transformDataPacient() {
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log('Transformando Dados');
+            // Implementar a transformação dos dados
+            const id_paciente = this.getPatientId();
+            const nome_paciente = this.getPatientName();
+            const idade_paciente = this.getPatientAge();
+            const email_paciente = this.getPatientEmail();
+            const telefone_paciente = this.getPatientPhone();
+            const endereco_paciente = this.getPatientAddress();
+            this.setCartaoCadastro("\nID: " + id_paciente + "\nO Paciente: " + nome_paciente + " foi cadastrado no sistema" +
+                "\nDados do Paciente: " +
+                "\n Nome: " + nome_paciente + " Idade: " + idade_paciente +
+                "\n Telefone: " + telefone_paciente + " Email: " + email_paciente +
+                "\n Endereco: " + endereco_paciente);
+            this.loadDataPacient();
+        });
+    }
+    loadDataPacient() {
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log('Carregando Dados para o Sistema');
+            // Implementar a carga dos dados para o sistema
+            // Exemplo: Salvar no banco de dados, gravar em um arquivo, etc.
+            console.log('Cartão de Cadastro: ', this.getCartaoCadastro());
+            console.log('Cartão de Cadastro Salvo com Sucesso');
+        });
+    }
+    getPatientId() {
+        return this.patientId;
+    }
+    getPatientName() {
+        return this.patientName;
+    }
+    getPatientAge() {
+        return this.patientAge;
+    }
+    getPatientPhone() {
+        return this.patientPhone;
+    }
+    getPatientEmail() {
+        return this.patientEmail;
+    }
+    getPatientAddress() {
+        return this.patientAddress;
+    }
+    getCartaoCadastro() {
+        return this.cartaoCadastro;
+    }
+    setPatientId(patient_id) {
+        this.patientId = patient_id;
+    }
+    setPatientName(name) {
+        this.patientName = name;
+    }
+    setPatientAge(age) {
+        this.patientAge = age;
+    }
+    setPatientPhone(phone) {
+        this.patientPhone = phone;
+    }
+    setPatientEmail(email) {
+        this.patientEmail = email;
+    }
+    setPatientAddress(address) {
+        this.patientAddress = address;
+    }
+    setCartaoCadastro(cartao_cadastro) {
+        this.cartaoCadastro = cartao_cadastro;
+    }
+}
---
+++ dist/dist/dist/database.js
@@ -0,0 +1,54 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try {
+            step(generator.next(value));
+        }
+        catch (e) {
+            reject(e);
+        } }
+        function rejected(value) { try {
+            step(generator["throw"](value));
+        }
+        catch (e) {
+            reject(e);
+        } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import * as mysql from 'mysql2';
+export class Database {
+    static init() {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!this.pool) {
+                this.pool = mysql.createPool({
+                    host: 'localhost',
+                    user: 'root',
+                    password: '6z2h1j3k9F!',
+                    database: 'cloud_server',
+                });
+            }
+        });
+    }
+    static query(queryText_1) {
+        return __awaiter(this, arguments, void 0, function* (queryText, params = []) {
+            return new Promise((resolve, reject) => {
+                this.pool.execute(queryText, params, (err, results) => {
+                    if (err) {
+                        console.error('Error executing query:', err);
+                        reject(err);
+                    }
+                    else {
+                        resolve(results);
+                    }
+                });
+            });
+        });
+    }
+    static close() {
+        if (this.pool) {
+            this.pool.end();
+        }
+    }
+}
