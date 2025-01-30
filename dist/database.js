867d24621e2a28ec2c76e34290d66031b3da45e9
Felipe Santos <owner@spacedwog.com.br>
2025-01-30 10:02:29 -0300

    Commited

--- dist/database.js
+++ dist/database.js
@@ -1,64 +1,26 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
+import * as mysql from 'mysql2';
+export class Database {
+    static pool;
+    static async init() {
+        if (!this.pool) {
+            this.pool = mysql.createPool({
+                host: 'localhost',
+                user: 'root',
+                password: '6z2h1j3k9F!',
+                database: 'cloud_server',
+            });
+        }
     }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Database = void 0;
-const mysql = __importStar(require("mysql2"));
-class Database {
-    static init() {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (!this.pool) {
-                this.pool = mysql.createPool({
-                    host: 'localhost',
-                    user: 'root',
-                    password: '6z2h1j3k9F!',
-                    database: 'cloud_server',
-                });
-            }
-        });
-    }
-    static query(queryText, params = []) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return new Promise((resolve, reject) => {
-                this.pool.execute(queryText, params, (err, results) => {
-                    if (err) {
-                        console.error('Error executing query:', err);
-                        reject(err);
-                    }
-                    else {
-                        resolve(results);
-                    }
-                });
+    static async query(queryText, params = []) {
+        return new Promise((resolve, reject) => {
+            this.pool.execute(queryText, params, (err, results) => {
+                if (err) {
+                    console.error('Error executing query:', err);
+                    reject(err);
+                }
+                else {
+                    resolve(results);
+                }
             });
         });
     }
@@ -68,4 +30,3 @@ class Database {
         }
     }
 }
-exports.Database = Database;
