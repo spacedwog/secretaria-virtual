0abf24929adbde8d7bc59d2e01b79c66afc309d9
Spacedwog <owner@spacedwog.com.br>
2025-05-20 08:11:40 -0300

    Commited

--- Cloud-dev/server.ts
+++ Cloud-dev/server.ts
@@ -1,11 +1,10 @@
 import * as net from 'net';
 import * as path from 'path';
-import express from 'express';
 import * as dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
-import * as bodyParser from 'body-parser';
 import * as mysql from 'mysql2/promise';
-import { Request, Response, NextFunction } from 'express';
+import * as bodyParser from 'body-parser';
+import express, { Request, Response, NextFunction } from 'express';
 import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
 
 dotenv.config();
