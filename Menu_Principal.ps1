f6234fc0ef329cda76951cac473b376def6f35a4
Spacedwog <owner@spacedwog.com.br>
2025-05-20 11:53:39 -0300

    Commited

---
+++ Menu_Principal.ps1
@@ -0,0 +1,61 @@
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+# Funções simuladas para os menus
+function Abrir-MenuPaciente {
+    # Aqui você pode chamar o MenuPaciente.ps1 ou chamar a função diretamente
+    & .\MenuPaciente.ps1
+}
+
+function Abrir-MenuConsultaMedica {
+    & .\MenuConsultaMedica.ps1
+}
+
+function Registrar-ReceitaMedica {
+    [System.Windows.Forms.MessageBox]::Show("Registrando Receita Médica...")
+}
+
+function Imprimir-ReceitaMedica {
+    [System.Windows.Forms.MessageBox]::Show("Imprimindo Receita Médica...")
+}
+
+# Janela principal
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Sistema de Secretaria Virtual"
+$form.Size = New-Object System.Drawing.Size(400,350)
+$form.StartPosition = "CenterScreen"
+
+# Botões
+$btn1 = New-Object System.Windows.Forms.Button
+$btn1.Text = "1. Menu Paciente"
+$btn1.Size = New-Object System.Drawing.Size(300,40)
+$btn1.Location = New-Object System.Drawing.Point(50,30)
+$btn1.Add_Click({ Abrir-MenuPaciente })
+
+$btn2 = New-Object System.Windows.Forms.Button
+$btn2.Text = "2. Menu Consulta Médica"
+$btn2.Size = New-Object System.Drawing.Size(300,40)
+$btn2.Location = New-Object System.Drawing.Point(50,80)
+$btn2.Add_Click({ Abrir-MenuConsultaMedica })
+
+$btn3 = New-Object System.Windows.Forms.Button
+$btn3.Text = "3. Receita Médica"
+$btn3.Size = New-Object System.Drawing.Size(300,40)
+$btn3.Location = New-Object System.Drawing.Point(50,130)
+$btn3.Add_Click({ Registrar-ReceitaMedica })
+
+$btn4 = New-Object System.Windows.Forms.Button
+$btn4.Text = "4. Imprimir Receita Médica"
+$btn4.Size = New-Object System.Drawing.Size(300,40)
+$btn4.Location = New-Object System.Drawing.Point(50,180)
+$btn4.Add_Click({ Imprimir-ReceitaMedica })
+
+$btn5 = New-Object System.Windows.Forms.Button
+$btn5.Text = "5. Sair"
+$btn5.Size = New-Object System.Drawing.Size(300,40)
+$btn5.Location = New-Object System.Drawing.Point(50,230)
+$btn5.Add_Click({ $form.Close() })
+
+$form.Controls.AddRange(@($btn1, $btn2, $btn3, $btn4, $btn5))
+
+[void]$form.ShowDialog()
