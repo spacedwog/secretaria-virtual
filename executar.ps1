73841d8110ef42cb8b26680ef7842766fd6013bd
Spacedwog <owner@spacedwog.com.br>
2025-05-21 15:45:58 -0300

    Commited

--- executar.ps1
+++ executar.ps1
@@ -1,5 +1,3 @@
-Add-Type -AssemblyName System.Windows.Forms
-
 # Caminho completo para o executável que você quer iniciar
 $exePath = "C:\Users\felip\secretaria-virtual\secretaria_virtual.exe"
 
@@ -20,25 +18,10 @@ catch {
     exit
 }
 
-$form = New-Object System.Windows.Forms.Form
-$form.Text = "Aguardando processo..."
-$form.Size = New-Object System.Drawing.Size(300,100)
-$form.StartPosition = "CenterScreen"
-
-# Exibe o form em uma thread diferente para não travar o script
-$null = [System.Threading.Thread]::New([System.Threading.ThreadStart]{
-    [System.Windows.Forms.Application]::Run($form)
-}).Start()
-
 # Aguarda o término do processo
 Write-Host "[ESPERA] Aguardando o processo terminar..."
 $processo.WaitForExit()
 
-# Fecha o formulário na thread da UI
-$form.Invoke([Action]{
-    $form.Close()
-})
-
 # Exibe a saída final
 Write-Host "[OK] O processo terminou em: $($processo.ExitTime)" -ForegroundColor Green
-Write-Host "Código de saída: $($processo.ExitCode)" -ForegroundColor Yellow
+Write-Host "Codigo de saida: $($processo.ExitCode)" -ForegroundColor Yellow
