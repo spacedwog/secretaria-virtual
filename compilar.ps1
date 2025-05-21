68382e95537668893679167cc734babd56a688f9
Spacedwog <owner@spacedwog.com.br>
2025-05-21 11:16:46 -0300

    Commited

--- compilar.ps1
+++ compilar.ps1
@@ -4,7 +4,7 @@ Invoke-PS2EXE `
   -Title "Secretaria Virtual" `
   -Description "IA - Software para uma secretaria virtual com Nuvem." `
   -Company "Spacedwog" `
-  -Product "Sistema Medico Virtual" `
+  -Product "Sistema de Secretaria Virtual" `
   -Version "1.0.0.0" `
   -Copyright "© 2025 Felipe Rodrigues dos Santos (felipersantos1988@gmail.com). Todos os direitos reservados." `
   -noConsole `
